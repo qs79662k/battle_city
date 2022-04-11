a826ef6ecff12eeef671d27e94ff3410d0dd4543
qs79662k <97105840+qs79662k@users.noreply.github.com>
2022-04-11 10:21:44 +0800

    Update Bullet.java

--- src/com/wsp/tank/spirit/Bullet.java
+++ src/com/wsp/tank/spirit/Bullet.java
@@ -11,7 +11,7 @@ import com.wsp.tank.sound.PlayerSound;
 public class Bullet extends Rectangle implements Spirit {
 
 	public int dir;
-	private int speed = 7;  //子弹移动速度
+	public final static int speed = 10;  //子弹移动速度
 	private int energy;  //子弹威力
 	public boolean isEnemy;	
 	private int on;	//是几号玩家的子弹，如为敌人坦克则此值无效
@@ -93,6 +93,7 @@ public class Bullet extends Rectangle implements Spirit {
 			new PlayerSound(Art.hitiron);
 			battleScene.bullets.remove(this);
 			Bomb.minBomb(battleScene, DIR_UP , x, y);
+			return false;
 		}
 		
 		int _x = (x - 32) / 8;
